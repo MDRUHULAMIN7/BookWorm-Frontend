b742947df575124133b36f19e4aade633c51e416
mdruhulamin7 <ruhulthisis@gmail.com>
2026-01-14 08:31:06 +0600

    Api Intrigrate in Admin Tutorial Page

--- app/(admin)/admin/tutorial/page.tsx
+++ app/(admin)/admin/tutorial/page.tsx
@@ -4,7 +4,7 @@ import TutorialsTable from "./_components/TutorialsTable";
 
 export default function AdminTutorialPage() {
   return (
-    <div className="p-6 bg-gray-50 min-h-screen">
+    <div className="p-2 lg:p-6 bg-gray-50 min-h-screen">
       <Header title="Tutorials Management" subtitle="Manage tutorial Create Update Delete " />
       <TutorialsTable />
     </div>
